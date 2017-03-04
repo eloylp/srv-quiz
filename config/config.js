f225caa62dd09fe344d3319cb357bd1157e43794
eloylp <eloy@sandboxwebs.com>
2017-03-04 21:04:58 +0100

    Added output formatting in json .

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = (function () {
     let config = {};
 
     config.xPoweredBy = false;
+    config.jsonSpaces = 40;
     config.authToken = process.env.AUTH_TOKEN || "c40f7f4d574fbd6cdb025ae076701d5a";
     config.environment = process.env.NODE_ENV || 'production';
     config.port = parseInt(process.env.PORT) || 8000;
