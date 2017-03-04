c56ce368c08d78bc454e4a8acc5d3335756adde6
eloylp <eloy@sandboxwebs.com>
2017-03-04 19:39:00 +0100

    Changes in app general config , making  auth token getted from ENV. Disabling server information.

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ module.exports = (function () {
 
     let config = {};
 
-    config.xPoweredBy = "quizServer";
-    config.authToken = "c40f7f4d574fbd6cdb025ae076701d5a";
+    config.xPoweredBy = false;
+    config.authToken = process.env.AUTH_TOKEN || "c40f7f4d574fbd6cdb025ae076701d5a";
     config.environment = process.env.NODE_ENV || 'production';
     config.port = parseInt(process.env.PORT) || 8000;
     return config;
